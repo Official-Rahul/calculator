89034a0fed43bedd57a596cd16b5890ddb027b7c
Official-Rahul <official.rahul.email@gmail.com>
2020-02-25 14:07:00 +0530

    gitignore | math calculation using mathjs

--- action.js
+++ action.js
@@ -2,3 +2,12 @@ function input(num) {
     var input=document.getElementById("inputArea");
     input.value+=num;
 }
+function calculate() {
+    var input=document.getElementById("inputArea");
+    var result=math.evaluate(input.value);
+    input.value+='\n='+result;
+}
+function blank() {
+    var input=document.getElementById("inputArea");
+    input.value='';
+}
